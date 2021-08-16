c92361db4885b89389a448ad417f345102bd2bcc
hasnaine <hasnaineali@icloud.com>
2021-08-16 17:27:40 +0200

    ex 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Steps from "./components/Steps";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0
+const stepsMax = 50000
+
+export class App extends React.Component {
+  render() {
+    return (
+
+      <div className="container-fluid">
+        <div className="row">
+          <p>
+            Heart : 80
+            Temperature : -20
+            Steps : 0
+          </p>
+          <Steps/>
+        </div>
+      </div>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Steps.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Steps extends React.Component {
+    render() {
+        return(
+            <h1>hello</h1>
+        )
+    }
+}
+
+export default Steps;
